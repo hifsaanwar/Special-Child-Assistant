75bb9690af6a11eb49ed3ceefe9ff1788984aad8
SarmadMasood <55407081+SarmadMasood@users.noreply.github.com>
2021-12-11 22:16:12 +0500

    grrrrrr
    grrrr
    
    Co-Authored-By: hifsaanwar <88451433+hifsaanwar@users.noreply.github.com>

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your app
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
